edc0c3c56360a74022fa39fb113b61efdf2254d2
thirtyThreeS <thaumasios@outlook.com>
2021-11-25 02:14:53 -0500

    initial commit

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace AreaOfCircle
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            double areaOfCircle = AreaOfCircle(radius);
+            Console.WriteLine($"The area of your circle with radius of {radius} is {areaOfCircle}");
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
